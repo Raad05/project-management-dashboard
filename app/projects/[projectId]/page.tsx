f1f4a6dcc038edd65645a745867bf0e3bc0f34f0
Raad05 <ashabulraad@gmail.com>
2024-05-01 23:19:18 +0600

    filtered projectDetails

--- app/projects/[projectId]/page.tsx
+++ app/projects/[projectId]/page.tsx
@@ -1,5 +1,22 @@
+"use client";
+
+import { Project } from "@/app/components/ProjectsPage/ProjectsPage";
+import { useProjectStore } from "@/app/store/projectStore";
+
 const ProjectDetails = ({ params }: { params: { projectId: string } }) => {
-  return <div className="product-details">{params.projectId}</div>;
+  const { projects } = useProjectStore((state: any) => ({
+    projects: state.projects,
+  }));
+
+  const projectById = projects.find(
+    (project: Project) => project.id === params.projectId
+  );
+
+  return (
+    <div className="product-details">
+      <p>{projectById.name}</p>
+    </div>
+  );
 };
 
 export default ProjectDetails;
