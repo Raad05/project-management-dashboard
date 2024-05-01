015f3c0317c5474e0ccef234c3f6eb93c086f6ed
Raad05 <ashabulraad@gmail.com>
2024-05-02 05:27:14 +0600

    final commit

--- README.md
+++ README.md
@@ -1,36 +1,18 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+# Project Management Dashboard
 
-## Getting Started
-
-First, run the development server:
+Run the following commands in terminal:
 
 ```bash
+# Install dependencies
+npm install
+# Start localhost server
 npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-# or
-bun dev
 ```
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+## Tech Stack
 
-You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
-
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
-
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+- Next JS for server-side rendering.
+- Tailwind CSS for styling.
+- Ant Design for pre-build UI components.
+- Zustand for state management.
+- React Query as the query lirary.
--- app/components/LoginPage/LoginPage.tsx
+++ app/components/LoginPage/LoginPage.tsx
@@ -2,6 +2,7 @@
 
 import type { FormProps } from "antd";
 import { Button, Checkbox, Form, Input } from "antd";
+import Link from "next/link";
 
 type FieldType = {
   username?: string;
@@ -10,13 +11,7 @@ type FieldType = {
 };
 
 const onFinish: FormProps<FieldType>["onFinish"] = (values) => {
-  if (values.username === "Yamin" && values.password === "123") {
-    console.log("Success:", values);
-    alert("Login successful!");
-  } else {
-    console.log("Username or password does not match");
-    alert("Username or password does not match");
-  }
+  console.log("Success:", values);
 };
 
 const onFinishFailed: FormProps<FieldType>["onFinishFailed"] = (errorInfo) => {
@@ -25,50 +20,54 @@ const onFinishFailed: FormProps<FieldType>["onFinishFailed"] = (errorInfo) => {
   errorInfo.errorFields.map((error) => alert(error.errors[0]));
 };
 
-const LoginPage = () => (
-  <div className="flex justify-center items-center h-screen">
-    <Form
-      name="basic"
-      labelCol={{ span: 8 }}
-      wrapperCol={{ span: 16 }}
-      style={{ maxWidth: 600 }}
-      initialValues={{ remember: true }}
-      onFinish={onFinish}
-      onFinishFailed={onFinishFailed}
-      autoComplete="off"
-    >
-      <p className="my-10 font-bold text-xl text-center">Login</p>
-      <Form.Item<FieldType>
-        label="Username"
-        name="username"
-        rules={[{ required: true, message: "Please input your username!" }]}
+const LoginPage = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <Form
+        name="basic"
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
+        style={{ maxWidth: 600 }}
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
       >
-        <Input />
-      </Form.Item>
+        <p className="my-10 font-bold text-xl text-center">Login</p>
+        <Form.Item<FieldType>
+          label="Username"
+          name="username"
+          rules={[{ required: true, message: "Please input your username!" }]}
+        >
+          <Input />
+        </Form.Item>
 
-      <Form.Item<FieldType>
-        label="Password"
-        name="password"
-        rules={[{ required: true, message: "Please input your password!" }]}
-      >
-        <Input.Password />
-      </Form.Item>
+        <Form.Item<FieldType>
+          label="Password"
+          name="password"
+          rules={[{ required: true, message: "Please input your password!" }]}
+        >
+          <Input.Password />
+        </Form.Item>
 
-      <Form.Item<FieldType>
-        name="remember"
-        valuePropName="checked"
-        wrapperCol={{ offset: 8, span: 16 }}
-      >
-        <Checkbox>Remember me</Checkbox>
-      </Form.Item>
+        <Form.Item<FieldType>
+          name="remember"
+          valuePropName="checked"
+          wrapperCol={{ offset: 8, span: 16 }}
+        >
+          <Checkbox>Remember me</Checkbox>
+        </Form.Item>
 
-      <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-        <Button type="primary" htmlType="submit">
-          Login
-        </Button>
-      </Form.Item>
-    </Form>
-  </div>
-);
+        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+          <Link href="/projects">
+            <Button type="primary" htmlType="submit">
+              Login
+            </Button>
+          </Link>
+        </Form.Item>
+      </Form>
+    </div>
+  );
+};
 
 export default LoginPage;
